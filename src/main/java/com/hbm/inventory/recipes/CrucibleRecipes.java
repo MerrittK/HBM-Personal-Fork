c9ea8a3b9d657982b42463aef2eb02e83e4c86d4
MerrittK <65367913+MerrittK@users.noreply.github.com>
2024-02-29 14:54:18 -0500

    oops

--- src/main/java/com/hbm/inventory/recipes/CrucibleRecipes.java
+++ src/main/java/com/hbm/inventory/recipes/CrucibleRecipes.java
@@ -84,7 +84,7 @@ public class CrucibleRecipes extends SerializableRecipe {
 				.inputs(new MaterialStack(Mats.MAT_STEEL, n * 5), new MaterialStack(Mats.MAT_TUNGSTEN, n * 3), new MaterialStack(Mats.MAT_COBALT, n * 1))
 				.outputs(new MaterialStack(Mats.MAT_DURA, n * 9)));
 		
-		recipes.add(new CrucibleRecipe(14, "crucible.stainles", 9, new ItemStack(ModItems.ingot_stainless))
+		recipes.add(new CrucibleRecipe(14, "crucible.stainless", 9, new ItemStack(ModItems.ingot_stainless))
 				.inputs(new MaterialStack(Mats.MAT_STEEL, n * 5), new MaterialStack(Mats.MAT_NICKEL, n * 3), new MaterialStack(Mats.MAT_COBALT, n * 1))
 				.outputs(new MaterialStack(Mats.MAT_STAINLESS, n * 9)));
 		
